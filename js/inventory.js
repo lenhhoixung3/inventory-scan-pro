4d6db2aee34d8cc62e248ac709591bbe2dab655b
lenhhoixung3 <luuductai.bds@gmail.com>
2026-03-13 00:33:19 +0700

    Create inventory.js

---
+++ js/inventory.js
@@ -0,0 +1,52 @@
+let products=[]
+
+function addProduct(barcode){
+
+let found=products.find(p=>p.barcode===barcode)
+
+if(found){
+
+found.qty++
+
+}else{
+
+products.push({
+
+barcode:barcode,
+name:"Unknown Product",
+qty:1
+
+})
+
+}
+
+renderInventory()
+updateDashboard()
+
+}
+
+function renderInventory(){
+
+const list=document.getElementById("inventoryList")
+
+list.innerHTML=""
+
+products.forEach(p=>{
+
+const div=document.createElement("div")
+
+div.className="product"
+
+div.innerHTML=`
+
+${p.name}<br>
+Barcode: ${p.barcode}<br>
+Qty: ${p.qty}
+
+`
+
+list.appendChild(div)
+
+})
+
+}
